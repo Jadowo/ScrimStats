e52a76c775473b61d4fea33bf5e69498b5d07016
Brett Bennett <bbennett905@gmail.com>
2019-06-20 22:08:14 -0500

    Styling

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+  $errorlog = $_SERVER['DOCUMENT_ROOT'] . "/error.log";
+  ini_set('display_errors', FALSE);
+  ini_set('log_errors', TRUE);
+  ini_set('error_log', $errorlog);
   $dbcfg = include('config.php');
   $connection = new mysqli($dbcfg['host'], $dbcfg['user'], $dbcfg['pass'], $dbcfg['dbname']);
-  $query = "SELECT auth_id,
+  $sql = "SELECT auth_id,
     name,
     COUNT(ps.demo_id) AS total_Games,
     SUM(rounds_played) AS total_Rounds,
@@ -21,33 +25,35 @@
     INNER JOIN demo_demos AS d
     ON ps.demo_id = d.demo_id
     GROUP BY auth_id";
-  $result = mysqli_query($connection, $query);
+  $result = $connection->query($sql);
 ?>
 <html>
   <head>
   <title>Scrim Stats</title>
-  <link rel="stylesheet" href="style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/cosmo/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.16/datatables.min.css" />
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.16/datatables.min.js"></script>
+  <link rel="shortcut icon" href="https://xenogamers.com/favicon.ico" />
   </head>
-  <style>
-    body {
-      background-color:#cacaca;
-    }
-  </style>
   <body>
-    <br /><br/>
-    <div class="container">
-      <h3>Scrim Stats</h3>
-      <br/>
-      <div class="table-responsive">
-        <table id="scrim_stats" cellspacing="0" class="cell-border hover order-column row-border" width="100%">
+    <div class="jumbotron shadow"><div class="container-fluid"><h1 style="width:75%;margin:auto;margin-top:0px;">xG Scrim Stats</h1></div></div>
+    <div class="container-fluid body-content text-center"><br />
+      <div style="margin:auto;margin-top:0px;">
+        <?php
+          if ($connection->connect_error) {
+            echo "<div class=\"alert alert-dismissible alert-danger\">\n";
+            echo "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>\n";
+            echo "<p class=\"mb-0\">Unable to connect to database. Data may be unavailable.</p>\n";
+            echo "</div>\n";
+          }
+        ?>
+        <table id="scrim_stats" cellspacing="0" class="stats-table table table-hover table-dark table-sm shadow" width="100%">
           <thead>
             <tr>
-              <td>Name</td>
+              <td>Player</td>
               <td>Games Played</td>
               <td>Rounds Played</td>
               <td>Total Damage</td>
@@ -64,36 +70,46 @@
               <td>Win Percent</td>
             </tr>
           </thead>
-          <?php
-          while($row = mysqli_fetch_array($result)){
-            echo '
-              <tr>
-              <td>'.$row["name"].'</td>
-              <td>'.$row["total_Games"].'</td>
-              <td>'.$row["total_Rounds"].'</td>
-              <td>'.$row["total_Dmg"].'</td>
-              <td>'.$row["total_Kills"].'</td>
-              <td>'.$row["total_Assists"].'</td>
-              <td>'.$row["total_Deaths"].'</td>
-              <td>'.$row["KDR"].'</td>
-              <td>'.$row["AKPR"].'</td>
-              <td>'.$row["AKPG"].'</td>
-              <td>'.$row["ADPR"].'</td>
-              <td>'.$row["ADPG"].'</td>
-              <td>'.$row["AKDR"].'</td>
-              <td>'.$row["WON"].'</td>
-              <td>'.$row["WIN_PERCENT"].'</td>
-              </tr>
-            ';
-          }
-          ?>
+          <tbody>
+            <?php
+              while ($row = mysqli_fetch_array($result)) {
+                echo '
+                  <tr>
+                  <td>' . $row["name"] . '</td>
+                  <td>' . $row["total_Games"] . '</td>
+                  <td>' . $row["total_Rounds"] . '</td>
+                  <td>' . $row["total_Dmg"] . '</td>
+                  <td>' . $row["total_Kills"] . '</td>
+                  <td>' . $row["total_Assists"] . '</td>
+                  <td>' . $row["total_Deaths"] . '</td>
+                  <td>' . round($row["KDR"], 2) . '</td>
+                  <td>' . round($row["AKPR"], 2) . '</td>
+                  <td>' . round($row["AKPG"], 2) . '</td>
+                  <td>' . round($row["ADPR"], 2) . '</td>
+                  <td>' . round($row["ADPG"], 2) . '</td>
+                  <td>' . round($row["AKDR"], 2) . '</td>
+                  <td>' . round($row["WON"], 2) . '</td>
+                  <td>' . round($row["WIN_PERCENT"], 2) . '</td>
+                  </tr>
+                ';
+              }
+            ?>
+          </tbody>
         </table>
       </div>
-     </div>
+    </div>
   </body>
+  <script>
+    $(document).ready(function() {
+      $('#scrim_stats').DataTable({
+        "order": [[3, "asc"]],
+        "searching": true,
+        "info": false,
+        "lengthChange": false,
+        "pageLength": 20,
+        "autoWidth":false,
+      });
+    });
+  </script>
+  <link rel="stylesheet" href="site.css" />
 </html>
-<script>
-$(document).ready(function() {
-  $('#scrim_stats').DataTable();
-});
-</script>
